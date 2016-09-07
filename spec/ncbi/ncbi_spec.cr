45f638ff43491649ce97561b82962ee8ed47dc7c
marciogm <marciogiaxa@gmail.com>
2016-09-07 16:16:13 -0300

    changed class API::NCBI to module
    also, refactor the way how a query is created

--- spec/ncbi/ncbi_spec.cr
+++ spec/ncbi/ncbi_spec.cr
@@ -1,35 +1,42 @@
 require "../spec_helper"
-include Biocr::Apis
+include Biocr::API
 
 describe Biocr::Apis do
-  it "search for DB and Term" do
-    ncbi = Biocr::Apis::NCBI.new
-    result = ncbi.search("sra", "papain")
+
+  it "create a url with proper options" do
+    result = NCBI.create_url("esearch.fcgi?db=sra&term=papain", {retmax: 10})
+    result.should eq("esearch.fcgi?db=sra&term=papain&retmode=json&retmax=10")
+  end
+
+  it "does search" do
+    result = NCBI.search("sra", "papain")
     result.size.should eq(20)
   end
 
+  it "search by retmax" do
+    result = NCBI.search("sra", "papain", retmax: 10)
+    result.size.should eq(10)
+  end
+
   it "get summary from search ids" do
-    ncbi = Biocr::Apis::NCBI.new
-    result = ncbi.search("popset", "Latrodectus katipo[Organism]")
+    result = NCBI.search("popset", "Latrodectus katipo[Organism]")
     result.size.should eq(6)
 
-    summary = ncbi.summary("popset", result)
+    summary = NCBI.summary("popset", result)
     summary.size.should eq(6)
   end
 
   it "raise error with invalid querys" do
-    ncbi = Biocr::Apis::NCBI.new
-    result = ncbi.search("popset", "Latrodectus katipo[Organism]")
+    result = NCBI.search("popset", "Latrodectus katipo[Organism]")
     expect_raises Exception, "cannot get document summary" do
-      ncbi.summary("biosample", result)
+      NCBI.summary("biosample", result)
     end
   end
 
   it "fetch term and donwload a fasta file" do
-    ncbi = Biocr::Apis::NCBI.new
-    result = ncbi.search("protein", "Latrodectus katipo[Organism]")
+    result = NCBI.search("protein", "Latrodectus katipo[Organism]")
 
-    summary = ncbi.fetch("protein", result, "fasta")
+    summary = NCBI.fetch("protein", result, "fasta")
     summary.includes?(">gi|167843271|gb|ACA03542.1|").should eq(true)
   end
 end
--- src/biocr-apis/ncbi/ncbi.cr
+++ src/biocr-apis/ncbi/ncbi.cr
@@ -2,89 +2,98 @@ require "http/client"
 require "json"
 
 module Biocr
-  module Apis
-    class NCBI
+  module API::NCBI
 
-      struct Summary
-        property uid, title
-        def initialize()
-          @uid = String.new
-          @title = String.new
-        end
+    struct Summary
+      property uid, title
+      def initialize()
+        @uid = String.new
+        @title = String.new
       end
+    end
 
-      BASE_URL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    BASE_URL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
-      def request(query)
-        query = query.gsub(" ", "%20")
-        response = HTTP::Client.get("#{BASE_URL}#{query}")
-        # TODO: handle possible errors
-        response.body
+    def self.request(query)
+      query = query.gsub(" ", "%20")
+      response = HTTP::Client.get("#{BASE_URL}#{query}")
+      # TODO: handle possible errors
+      response.body
+    end
+
+    # Returns a list of primary IDS
+    #
+    # See the online documentations for more explanations
+    # [ESearch](http://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch)
+    #
+    # Example:
+    # ```
+    # NCBI.search("protein", "Latrodectus katipo[Organism]")
+    # => ["167843271", "167843269", "167843267"....]
+    #
+    def self.search(db : String, term : String, **options)
+      query = "esearch.fcgi?db=#{db}&term=#{term}&retmode=json"
+      query = create_url(query, options)
+
+      response = JSON.parse(request(query))
+
+      count = response["esearchresult"]["count"].as_s
+      ids = response["esearchresult"]["idlist"]
+
+      if count.to_i > 0
+        ids.to_a
+      else
+        [] of String
       end
+    end
 
-      # Returns a list of primary IDS
-      #
-      # See the online documentations for more explanations
-      # [ESearch](http://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch)
-      #
-      # Example:
-      # ```
-      # NCBI.search("protein", "Latrodectus katipo[Organism]")
-      # => ["167843271", "167843269", "167843267"....]
-      #
-      def search(db : String, term : String, **options)
-        query = "esearch.fcgi?db=#{db}&term=#{term}&retmode=json"
-        response = JSON.parse(request(query))
+    # Returns document summaries from a list of IDs
+    #
+    # See the online documentations for more explanations
+    # [ESummary](http://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESummary)
+    #
+    # **Parameters**
+    #
+    # **DB:** Database from which to retrieve DocSums. The value must be a valid Entrez database name.
+    #
+    # **ids:** UID list. Either a single UID or a comma-delimited list of UIDs may be provided.
+    #          All of the UIDs must be from the database specified by DB.
+    #
+    def self.summary(db, ids, **options)
+      query = "esummary.fcgi?db=#{db}&id=#{ids.join(",")}"
+      query = create_url(query, options)
 
-        count = response["esearchresult"]["count"].as_s
-        ids = response["esearchresult"]["idlist"]
+      response = JSON.parse(request(query))
+      uids = response["result"]["uids"]
 
-        if count.to_i > 0
-          ids.to_a
+      result = uids.each_with_object(Array(Summary).new) do |uid, result|
+        uid = uid.as_s
+        if response["result"][uid].includes?("error")
+          raise response["result"][uid]["error"].as_s
         else
-          [] of String
+          summary = Summary.new
+          summary.title = response["result"][uid]["title"].as_s
+          summary.uid = response["result"][uid]["title"].as_s
+          result << summary
         end
       end
+      result
+    end
 
-      # Returns document summaries from a list of IDs
-      #
-      # See the online documentations for more explanations
-      # [ESummary](http://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESummary)
-      #
-      # **Parameters**
-      #
-      # **DB:** Database from which to retrieve DocSums. The value must be a valid Entrez database name.
-      #
-      # **ids:** UID list. Either a single UID or a comma-delimited list of UIDs may be provided.
-      #          All of the UIDs must be from the database specified by DB.
-      #
-      # **ret_mode:** By default, the summary will return a **JSON**, but you can specify an **XML**
-      #
-      def summary(db, ids, ret_mode = "json")
-        query = "esummary.fcgi?db=#{db}&id=#{ids.join(",")}&retmode=#{ret_mode}"
+    def self.fetch(db, ids, ret_mode = "fasta")
+      query = "efetch.fcgi?db=#{db}&id=#{ids.join(",")}&rettype=#{ret_mode}"
+      response = request(query)
+    end
 
-        response = JSON.parse(request(query))
-        uids = response["result"]["uids"]
-
-        result = uids.each_with_object(Array(Summary).new) do |uid, result|
-          uid = uid.as_s
-          if response["result"][uid].includes?("error")
-            raise response["result"][uid]["error"].as_s
-          else
-            summary = Summary.new
-            summary.title = response["result"][uid]["title"].as_s
-            summary.uid = response["result"][uid]["title"].as_s
-            result << summary
-          end
+    def self.create_url(query, options)
+      opt = String.build do |io|
+        # if don't provide a retmode, will set a JSON by default
+        io << "&retmode=json" unless options.has_key?(:retmode)
+        options.each do |k,v|
+          io << "&#{k}=#{v}"
         end
-        result
       end
-
-      def fetch(db, ids, ret_mode = "fasta")
-        query = "efetch.fcgi?db=#{db}&id=#{ids.join(",")}&rettype=#{ret_mode}"
-        response = request(query)
-      end
-
+      query + opt
     end
   end
 end
